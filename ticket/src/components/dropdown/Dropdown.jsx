6f10a0bd61ba8fc4730a4aa236b1db3ded47af0c
sophiezhangg <zir.zhang@utexas.edu>
2022-11-15 19:56:44 -0600

    changed dropdown color and size

--- ticket/src/components/dropdown/Dropdown.jsx
+++ ticket/src/components/dropdown/Dropdown.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Dropdown2 from 'react-bootstrap/Dropdown'
-
+//import "./Dropdown.css"
+import "./Dropdown.scss";
 const Dropdown = () => {
   return (
  
     <div class = "container-fluid">
         <Dropdown2>
-      <Dropdown2.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown2.Toggle>
+        <div class="d-grid gap-2">
+          <Dropdown2.Toggle variant = "primary"  size = "lg">
+            Dropdown Button
+          </Dropdown2.Toggle>
+      </div>
 
       <Dropdown2.Menu>
         <Dropdown2.Item href="#/action-1">Action</Dropdown2.Item>
