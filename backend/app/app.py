c7a52a09840b98f0bb8b2e336ec09bc2cb93143f
Priya <pbarve@utexas.edu>
2022-11-16 12:48:26 -0600

    Location -> Attractions working with filtering

--- backend/app/app.py
+++ backend/app/app.py
@@ -30,8 +30,6 @@ def close_connection(cursor, connection):
 # Function to get ALL values in a table from our database
 # Pass in either "locations" or "attractions" as the value
 # for our table argument
-
-
 def get_all(table):
     config = {
         'user': 'root',
@@ -52,6 +50,63 @@ def get_all(table):
     connection.close()
 
     return result
+
+# what is the query we are trying to build? 
+# SELECT * FROM {table} WHERE id IN {(3, 4, 5)} AND {params}
+
+def build_query_new(table, id_list, request_params):
+    filters = {
+        "price_low",
+        "price_high",
+        "age_low",
+        "age_high",
+        "activity_type",
+        "time_spent",
+        "date",
+        "time_of_day"
+    }
+    low_limit_filters = {
+        "price_low",
+        "age_low"
+    }
+    high_limit_filters = {
+        "price_high",
+        "age_high"
+    }
+
+    query = f"SELECT * FROM {table}"
+    flag = False
+    # "SELECT * FROM {table} WHERE cid = '{cid}' AND id = '{id}' AND activity_type = 'Outdoor'"
+
+    for i in range (len(id_list)):
+        if(not flag): 
+            query += " WHERE id IN ("
+            flag = True
+        else:
+            query += ", "
+        id = id_list[i]
+        query += f"{id}"
+    if(flag):
+        query += ")"
+    
+    for key in request_params : 
+        if(not flag): 
+            query += " WHERE "
+            flag = True
+        else:
+            query += " AND " 
+        operator = "="
+        quotes = "'"
+        if key in low_limit_filters:
+            operator = ">="
+            quotes = ""
+        elif key in high_limit_filters:
+            operator = "<=" 
+            quotes = ""
+        query += (f"{key} {operator} {quotes}{request_params[key]}{quotes}") #id = '1'cid = '0'activity_type = 'Outdoor'
+    return query
+
+
 
 # function to get data on a specific location or attraction
 # using its location ID or attraction ID
@@ -78,6 +133,8 @@ def get_from_id(table, id):
     connection.close()
     return result 
 
+
+## I don't think we need this
 def get_from_city(table, cid):
     config = {
         'user': 'root',
@@ -125,7 +182,32 @@ def get_attractions_filters(request_params):
     return filters
 
 
+def run_query_new(query):
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'aet_tickets'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+
+    cursor.execute(query)
+    result = dict()
+    counter = 0
+    for row in cursor:
+        result[counter] = row # id used to be in place of counter
+        counter += 1
+
+    cursor.close()
+    connection.close()
+    return result 
+
+
+
 def run_query(table, request_params):
+
     # query = build_query(table, request_params)
     query = build_query(table, request_params)
     config = {
@@ -231,12 +313,21 @@ def get_attractions() -> str:
 ### -- MAIN ENTRY POINT
 def get_attractions_from_city(cid) -> str:
     # 1. Get from id from the locations table -> should output a list of cities that match the ID given (should be one city only--> list of size 1)
+    location_info = get_from_id('locations', cid)
+    request_params = request.data
+    filters = get_attractions_filters(request_params)
+    for key in location_info: 
+        aid_string = location_info[key][3]
+        aid_list = aid_string.strip('][').split(', ')
+        query = build_query_new('attractions', aid_list, filters)
+        return json.dumps(run_query_new(query))
+
     # 2. Parse the information to extract the Attractions List, and convert the string into a list of numbers
     # 3. Loop through the list of attractions and build a query with all the Attraction Id's 
     # 4. Query attractions from attractions table
     # 5. Filter attractions based on request args
     # 6. Return attractions
-    return json.dumps(get_from_city('attractions', cid))
+    #return json.dumps(get_from_city('attractions', cid))
 
 @app.route('/attractions/<id>')
 def get_attraction(id) -> str:
