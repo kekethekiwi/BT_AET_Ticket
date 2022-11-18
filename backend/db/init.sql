4c2ec211c116821c128c263fcb4eff80dabaca0e
Priya <pbarve@utexas.edu>
2022-11-18 16:13:23 -0600

    updated csv files, put 'id' field first

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -10,12 +10,13 @@ LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/data_files/locations.csv'
 INTO TABLE aet_tickets.locations
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
+LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (id,city,country,attraction_id_list);
 
 
 CREATE TABLE IF NOT EXISTS aet_tickets.attractions(
+  id, SMALLINT(7),
   `name` VARCHAR(50),
   price_low SMALLINT(7) UNSIGNED,
   price_high SMALLINT(7) UNSIGNED,
@@ -24,7 +25,6 @@ CREATE TABLE IF NOT EXISTS aet_tickets.attractions(
   activity_type VARCHAR(50),
   `address` TEXT(65535), 
   time_spent SMALLINT (7),
-  id SMALLINT(7),
   cid SMALLINT(7),
   `date` TEXT(65535),
   time_of_day TEXT(65535)
@@ -34,7 +34,7 @@ LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/data_files/attractions.csv'
 INTO TABLE aet_tickets.attractions
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
+LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(name,price_low,price_high,age_low,age_high,activity_type,address,time_spent,id,cid,date,time_of_day);
+(id,name,price_low,price_high,age_low,age_high,activity_type,address,time_spent,cid,date,time_of_day);
 
